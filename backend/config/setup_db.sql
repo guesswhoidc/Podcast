365a1fe74bf8b116c0225b6b9f8a2f714be3f5ba
Mauricio <mauricio.fidalgo1+gh@gmail.com>
2025-03-22 04:41:21 +0000

    adding comments, making the streams bigger and adding safeguards to only run if the tables don't exist

--- backend/config/setup_db.sql
+++ backend/config/setup_db.sql
@@ -1,61 +1,71 @@
-CREATE TABLE users (
+-- Create users table
+CREATE TABLE IF NOT EXISTS users (
   id INT PRIMARY KEY AUTO_INCREMENT,
-  display_name VARCHAR(255) NOT NULL,
-  passkey VARCHAR(255) NOT NULL,
-  email VARCHAR(255) UNIQUE NOT NULL,
+  display_name VARCHAR(512) NOT NULL,
+  passkey VARCHAR(1024) NOT NULL,
+  email VARCHAR(512) UNIQUE NOT NULL,
   created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
   updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 );
 
-CREATE TABLE podcasts (
+-- Create podcasts table
+CREATE TABLE IF NOT EXISTS podcasts (
   id INT PRIMARY KEY AUTO_INCREMENT,
-  name VARCHAR(255) NOT NULL,
-  thumbnail_url VARCHAR(255),
-  slug VARCHAR(255) UNIQUE NOT NULL,
+  name VARCHAR(512) NOT NULL,
+  thumbnail_url VARCHAR(1024),
+  slug VARCHAR(512) UNIQUE NOT NULL,
   created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
   last_indexed_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   FULLTEXT (name, slug)
 );
 
-CREATE TABLE episodes (
+-- Create episodes table
+CREATE TABLE IF NOT EXISTS episodes (
   id INT PRIMARY KEY AUTO_INCREMENT,
-  slug VARCHAR(255) UNIQUE NOT NULL,
-  title VARCHAR(255) NOT NULL,
+  slug VARCHAR(512) UNIQUE NOT NULL,
+  title VARCHAR(512) NOT NULL,
   description TEXT,
-  thumbnail_url VARCHAR(255),
-  episode_url VARCHAR(255) NOT NULL,
+  thumbnail_url VARCHAR(1024),
+  episode_url VARCHAR(1024) NOT NULL,
   length INT,
   last_indexed_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   podcast_id INT,
-  FOREIGN KEY (podcast_id) REFERENCES Podcasts(id)
+  FOREIGN KEY (podcast_id) REFERENCES podcasts(id)
 );
 
-CREATE TABLE episode_listens (
+-- Create episode_listens table
+CREATE TABLE IF NOT EXISTS episode_listens (
   id INT PRIMARY KEY AUTO_INCREMENT,
   user_id INT,
   episode_id INT,
   time_stamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-  FOREIGN KEY (user_id) REFERENCES Users(id),
-  FOREIGN KEY (episode_id) REFERENCES Episodes(id)
+  FOREIGN KEY (user_id) REFERENCES users(id),
+  FOREIGN KEY (episode_id) REFERENCES episodes(id)
 );
-CREATE TABLE playlists (
+
+-- Create playlists table
+CREATE TABLE IF NOT EXISTS playlists (
   id INT PRIMARY KEY AUTO_INCREMENT,
   user_id INT,
-  name VARCHAR(255) NOT NULL,
+  name VARCHAR(512) NOT NULL,
   created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-  FOREIGN KEY (user_id) REFERENCES Users(id)
+  FOREIGN KEY (user_id) REFERENCES users(id)
 );
-CREATE TABLE playlist_episodes (
+
+-- Create playlist_episodes table
+CREATE TABLE IF NOT EXISTS playlist_episodes (
   id INT PRIMARY KEY AUTO_INCREMENT,
   playlist_id INT,
   episode_id INT,
-  FOREIGN KEY (playlist_id) REFERENCES Playlists(id),
-  FOREIGN KEY (episode_id) REFERENCES Episodes(id)
+  FOREIGN KEY (playlist_id) REFERENCES playlists(id),
+  FOREIGN KEY (episode_id) REFERENCES episodes(id)
 );
-CREATE TABLE subscriptions (
+
+-- Create subscriptions table
+CREATE TABLE IF NOT EXISTS subscriptions (
   id INT PRIMARY KEY AUTO_INCREMENT,
   user_id INT,
   podcast_id INT,
-  FOREIGN KEY (user_id) REFERENCES Users(id),
-  FOREIGN KEY (podcast_id) REFERENCES Podcasts(id)
+  FOREIGN KEY (user_id) REFERENCES users(id),
+  FOREIGN KEY (podcast_id) REFERENCES podcasts(id)
 );
