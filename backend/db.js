0f6bc1d4a685980c181326f8ac0f0798a21c5a1c
Mauricio <mauricio.fidalgo1+gh@gmail.com>
2025-03-25 15:57:23 +0000

    Setting up dotenv for settings

--- backend/db.js
+++ backend/db.js
@@ -1,17 +1,29 @@
 import {Sequelize} from 'sequelize';
 export default async function () {
-    const db = new Sequelize('Podcast', 'podcast_app_user', 'app_password', {
-        host: 'localhost',
-        dialect: 'mysql'
-    });
+  let {
+    DB_NAME, 
+    DB_USERNAME, 
+    DB_PASSWORD,
+    DB_HOST
+  } = process.env;
 
-    try {
-        await db.authenticate();
-        console.log("db initialized");
-    } catch (error) {
-        console.error("Unable to connect to the database", error);
-        throw error;
-    }
+  console.assert(!!DB_NAME, "Make sure that the DB_NAME environment variable is set");
+  console.assert(!!DB_USERNAME, "Make sure that the DB_USERNAME environment variable is set");
+  console.assert(!!DB_PASSWORD, "Make sure that the DB_PASSWORD environment variable is set");
+  console.assert(!!DB_HOST, "Make sure that the DB_HOST environment variable is set");
 
-    return db;
+  const db = new Sequelize(DB_NAME, DB_USERNAME, DB_PASSWORD, {
+    host: DB_HOST,
+    dialect: 'mysql'
+  });
+
+  try {
+    await db.authenticate();
+    console.log("db initialized");
+  } catch (error) {
+    console.error("Unable to connect to the database", error);
+    throw error;
+  }
+
+  return db;
 }
--- backend/index.js
+++ backend/index.js
@@ -1,15 +1,18 @@
-import express from "express";
+import express from 'express';
+import dotenv from 'dotenv';
 import DB from './db.js';
+
+dotenv.config();
 const app = express();
-const PORT = 3052;
+const PORT = process.env.PORT || 3052;
 
 app.get("/", (req, res) => {
-    res.send("Hullo \n");
-    console.log("got request");
+  res.send("Hullo \n");
+  console.log("got request");
 });
 
 DB().then(() => {
-    app.listen(PORT, () => {
-        console.log(`server running at ${PORT}`);
-    });
+  app.listen(PORT, () => {
+    console.log(`server running at ${PORT}`);
+  });
 });
